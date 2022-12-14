00e0174bb304fb1cea3a835aed1e4ab0f5b82d34
manu <emanuelepires2000@gmail.com>
2022-12-14 17:27:45 -0300

    Implementando conversão de horas/minutos/segundos.

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,17 +1,17 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
-import style from './Cronometro.module.scss';
+import style from "./Cronometro.module.scss";
+import { tempoParaSegundos } from "../../common/utils/date";
 
 export default function Cronometro() {
+  console.log("conversão: ", tempoParaSegundos("01:01:01"));
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
       <div className={style.relogioWrapper}>
         <Relogio />
       </div>
-      <Botao>
-        Começar!
-      </Botao>
+      <Botao>Começar!</Botao>
     </div>
-  )
+  );
 }
